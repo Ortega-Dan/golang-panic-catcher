fc90af12e602ee74e8822170a093a2cab6315f9b
Dan Ortega <ortega.dan2010@gmail.com>
2021-03-18 18:19:33 -0500

    Adding CatchAndCall (catch to callback), and CatchToFile (catch to file or stdout)

--- gpanic/panicCatcher.go
+++ gpanic/panicCatcher.go
@@ -14,3 +14,25 @@ func Catch() {
 		debug.PrintStack()
 	}
 }
+
+// Catches a panic (recovers from it) and calls a given callback function,
+// preserving a convenience relation through an arbitrary string identifier
+// passed as the first argument
+func CatchAndCall(relationId string, fcallback func(relationId string, errorMsg string, stacktrace string)) {
+	if err := recover(); err != nil {
+		errorMsg := fmt.Errorf("%v", err).Error()
+		stackString := string(debug.Stack())
+
+		fcallback(relationId, errorMsg, stackString)
+	}
+}
+
+// Catches a panic (recovers from it) and prints it to a given file
+// it can be used to print to stdout by passing os.Stdout as argument
+func CatchToFile(file *os.File) {
+	if err := recover(); err != nil {
+		fmt.Fprintln(file, "")
+		fmt.Fprintln(file, err)
+		fmt.Fprintln(file, debug.Stack())
+	}
+}
